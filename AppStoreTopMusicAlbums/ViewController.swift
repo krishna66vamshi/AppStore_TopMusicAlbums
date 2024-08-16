c31345fadcff22e469694e49c850834729ac9261
vamsi <34568037+krishna66vamshi@users.noreply.github.com>
2024-08-16 20:03:17 +0530

    Project Created

---
+++ AppStoreTopMusicAlbums/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStoreTopMusicAlbums
+//
+//  Created by Hyper Thread Solutions Pvt Ltd on 16/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ AppStoreTopMusicAlbumsUITests/AppStoreTopMusicAlbumsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AppStoreTopMusicAlbumsUITestsLaunchTests.swift
+//  AppStoreTopMusicAlbumsUITests
+//
+//  Created by Hyper Thread Solutions Pvt Ltd on 16/08/24.
+//
+
+import XCTest
+
+final class AppStoreTopMusicAlbumsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
